be9b2f8030373ced66552bb3c96991c54f6f2263
Nikos Tsipinakis <nikos@tsipinakis.com>
2019-04-15 12:39:56 +0300

    Add plain event print x11 debug

--- src/x11/x.c
+++ src/x11/x.c
@@ -265,6 +265,110 @@ gboolean x_mainloop_fd_check(GSource *source)
         return XPending(xctx.dpy) > 0;
 }
 
+static void x_print_debug(XEvent ev) {
+        switch (ev.type) {
+                case KeyPress:
+                        printf("KeyPress\n");
+                        break;
+                case KeyRelease:
+                        printf("KeyRelease\n");
+                        break;
+                case ButtonPress:
+                        printf("ButtonPress\n");
+                        break;
+                case ButtonRelease:
+                        printf("ButtonRelease\n");
+                        break;
+                case MotionNotify:
+                        printf("MotionNotify\n");
+                        break;
+                case EnterNotify:
+                        printf("EnterNotify\n");
+                        break;
+                case LeaveNotify:
+                        printf("LeaveNotify\n");
+                        break;
+                case FocusIn:
+                        printf("FocusIn\n");
+                        break;
+                case FocusOut:
+                        printf("FocusOut\n");
+                        break;
+                case KeymapNotify:
+                        printf("KeymapNotify\n");
+                        break;
+                case Expose:
+                        printf("Expose\n");
+                        break;
+                case GraphicsExpose:
+                        printf("GraphicsExpose\n");
+                        break;
+                case NoExpose:
+                        printf("NoExpose\n");
+                        break;
+                case CirculateRequest:
+                        printf("CirculateRequest\n");
+                        break;
+                case ConfigureRequest:
+                        printf("ConfigureRequest\n");
+                        break;
+                case MapRequest:
+                        printf("MapRequest\n");
+                        break;
+                case ResizeRequest:
+                        printf("ResizeRequest\n");
+                        break;
+                case CirculateNotify:
+                        printf("CirculateNotify\n");
+                        break;
+                case ConfigureNotify:
+                        printf("ConfigureNotify\n");
+                        break;
+                case CreateNotify:
+                        printf("CreateNotify\n");
+                        break;
+                case DestroyNotify:
+                        printf("DestroyNotify\n");
+                        break;
+                case GravityNotify:
+                        printf("GravityNotify\n");
+                        break;
+                case MapNotify:
+                        printf("MapNotify\n");
+                        break;
+                case MappingNotify:
+                        printf("MappingNotify\n");
+                        break;
+                case ReparentNotify:
+                        printf("ReparentNotify\n");
+                        break;
+                case UnmapNotify:
+                        printf("UnmapNotify\n");
+                        break;
+                case VisibilityNotify:
+                        printf("VisibilityNotify\n");
+                        break;
+                case ColormapNotify:
+                        printf("ColormapNotify\n");
+                        break;
+                case ClientMessage:
+                        printf("ClientMessage\n");
+                        break;
+                case PropertyNotify:
+                        printf("PropertyNotify\n");
+                        break;
+                case SelectionClear:
+                        printf("SelectionClear\n");
+                        break;
+                case SelectionNotify:
+                        printf("SelectionNotify\n");
+                        break;
+                case SelectionRequest:
+                        printf("SelectionRequest\n");
+                        break;
+        }
+}
+
 /*
  * Main Dispatcher for XEvents
  */
@@ -278,7 +382,7 @@ gboolean x_mainloop_fd_dispatch(GSource *source, GSourceFunc callback, gpointer 
         unsigned int state;
         while (XPending(xctx.dpy) > 0) {
                 XNextEvent(xctx.dpy, &ev);
-
+                x_print_debug(ev);
                 switch (ev.type) {
                 case Expose:
                         LOG_D("XEvent: processing 'Expose'");
